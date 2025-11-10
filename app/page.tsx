5c14e9f1f7252eed2b0ea97f3d68af897f0d9ab5
jonathanrodriguesgermano <jonathanrodriguesgermano7@gmail.com>
2025-11-09 22:14:50 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Hearder from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Hearder />
       <div className="space-y-4 px-5">
         <SearchInput />
@@ -15,8 +16,15 @@ const Home = () => {
           sizes="100vw"
           className="h-auto w-full"
         />
+        <h2 className="text-xs text-foreground">Agendamentos</h2>
+        <BookingItem 
+        serviceName="Corte de Cabelo"
+        barbeshopName="Barbearia do João"
+        barbeshopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+        date={new Date()}
+        />
       </div>
-    </div>
+    </main>
   );
 };
 
