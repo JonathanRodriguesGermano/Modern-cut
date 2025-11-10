c19939cb4f3172d8c08ff6f854fb90e2e382f6bd
jonathanrodriguesgermano <jonathanrodriguesgermano7@gmail.com>
2025-11-09 21:08:24 -0300

    feat: add header and image components

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Hearder = () => {
+  return (
+    <header className="flex items-center justify-between bg-white px-5 py-6">
+      <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
+
+      <Button variant="outline" size="icon">
+        <MenuIcon />
+      </Button>
+    </header>
+  );
+};
+
+export default Hearder;
