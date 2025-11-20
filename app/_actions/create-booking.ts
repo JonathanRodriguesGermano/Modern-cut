b7eb9ac927cd8fbec2d7e653ea8c672ecf5f6088
jonathanrodriguesgermano <jonathanrodriguesgermano7@gmail.com>
2025-11-20 15:57:48 -0300

    feat: ensure booking can be created

---
+++ app/_actions/create-booking.ts
@@ -0,0 +1,60 @@
+"use server";
+import { actionClient } from "@/lib/action-client";
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { returnValidationErrors } from "next-safe-action";
+import { headers } from "next/headers";
+import { z } from "zod";
+
+// This schema is used to validate input from client.
+const inputSchema = z.object({
+  serviceId: z.uuid(),
+  date: z.date(),
+});
+
+export const createBooking = actionClient
+  .inputSchema(inputSchema)
+  .action(async ({ parsedInput: { serviceId, date} }) => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if(!session?.user) {
+        returnValidationErrors(inputSchema, {
+            _errors: ["User not authenticated"],
+        });
+    }
+    const service = await prisma.barbershopService.findUnique({
+        where: {
+            id: serviceId
+        },
+    });
+
+    if(!service) {
+        returnValidationErrors(inputSchema, {
+            _errors: ["Service not found"],
+        });
+    }
+
+    // verificar se ja existe um agendamento para essa data
+    const existingBooking = await prisma.booking.findFirst({
+        where: {
+            barbershopId: service.barebrshopId,
+            date,
+        },
+    });
+    if(existingBooking) {
+        console.error("Já existe um agendamento para essa data");
+        returnValidationErrors(inputSchema, {
+            _errors: ["Já existe um agendamento para essa data"],
+        });
+    }
+    const booking = await prisma.booking.create({
+        data: {
+            serviceId,
+            date,
+            userId: session.user.id,
+            barbershopId: service.barebrshopId,
+        },
+    });
+    return booking;
+  });
